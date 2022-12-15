054a37a506c26a52e34ac179563e8c66433d876c
lxj <lxj@qq.com>
2022-12-15 15:40:32 +0800

    hot-fix v2

--- src/main/java/cn/li/git/GitTest.java
+++ src/main/java/cn/li/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,git2!");
         System.out.println("hello,git3!");
         System.out.println("hot-fix 添加了代码");
+        System.out.println("hot-fix 又添加了代码");
     }
 }
